6ef8cb4b399fd723edf09979db0b733d6c10f9b3
choisungwook <kgg1959@naver.com>
2015-04-25 14:40:27 +0900

    Update Main3.c

--- Main3/Main3.c
+++ Main3/Main3.c
@@ -7,10 +7,13 @@ int __stdcall f2(int a, int b) { printf("f2 : %d %d\n", a, b); }
 int __fastcall f3(int a, int b) { printf("f3 : %d %d\n", a, b); }
 int __fastcall f4(int a, int b,int c) { printf("f4 : %d %d %d\n", a, b,c); }
 
+int asm_main();
+
 int main()
 {
-	f1(1, 2);
-	f2(1, 2);
-	f3(1, 2);
-	f1(1, 2,3);
+	asm_main();
+	//f1(1, 2);
+	//f2(1, 2);
+	//f3(1, 2);
+	//f1(1, 2,3);
 }

e91641e1eaf2c7a94413641bc09c56f43a982adc
choisungwook <kgg1959@naver.com>
2015-04-25 14:23:05 +0900

    Create Main3.c

---
+++ Main3/Main3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+//__cdecl은 생략가능
+
+int __cdecl f1(int a, int b) { printf("f1 : %d %d\n", a, b); }
+int __stdcall f2(int a, int b) { printf("f2 : %d %d\n", a, b); }
+int __fastcall f3(int a, int b) { printf("f3 : %d %d\n", a, b); }
+int __fastcall f4(int a, int b,int c) { printf("f4 : %d %d %d\n", a, b,c); }
+
+int main()
+{
+	f1(1, 2);
+	f2(1, 2);
+	f3(1, 2);
+	f1(1, 2,3);
+}
